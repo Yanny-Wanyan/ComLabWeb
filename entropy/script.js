f49ff921cf8203dbdbb3d98cdf56635c2471e525
Wanyan Yu <wy2478@nyu.edu>
2025-11-09 17:55:20 +0800

    finish

--- entropy/script.js
+++ entropy/script.js
@@ -17,11 +17,16 @@ function createNotification() {
     // console.log("button was click");
     return notification;
 }
-function createBulletingNote(){
+function createBulletingNote() {
     let bulletingNote = document.createElement("p");
     bulletingNote.innerText = "missCall";
     bulletingNote.className = "bulletingNote";
     bulletingNote.style.position = "absolute";
+    //     let r = Math.floor(Math.random() * 256);
+    // let g = Math.floor(Math.random() * 256);
+    // let b = Math.floor(Math.random() * 256);
+    // bulletingNote.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+    // bulletingNote.style.color = "rgb(" + r + "," + g + "," + b + ")";
     document.querySelector("body").append(bulletingNote);
     return bulletingNote;
 }
@@ -33,6 +38,10 @@ function drifting(notificationElement) {
     let startY = (Math.random() - 0.5) * 2000;
     let startR = (Math.random() - 0.5) * 50;
     let driftSpeed = Math.random() * 4 + 3;
+    // let r = Math.floor(Math.random() * 256);
+    // let g = Math.floor(Math.random() * 256);
+    // let b = Math.floor(Math.random() * 256);
+    // notificationElement.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
     notificationElement.style.setProperty('--drift-x', driftX + 'px');
     notificationElement.style.setProperty('--drift-y', driftY + 'px');
     notificationElement.style.setProperty('--drift-r', driftR + 'deg');
@@ -43,7 +52,7 @@ function drifting(notificationElement) {
 }
 function StartDriftingAll() {
     // select all items and apply the function into each of them
-   let bulletingNote = createBulletingNote();
+    let bulletingNote = createBulletingNote();
     let allDriftingNotice = document.querySelectorAll(".bulletingNote");
     allDriftingNotice.forEach(drifting);
     console.log("start drifting applied to all.");
@@ -52,14 +61,12 @@ function pushing(item) {
     // when notices fill the phone, they start pushing and pulling each other
     // use random to set the pushing distance
     // select all items and apply the function into each of them
-    let pushY = Math.random() * 30 - 5;
-    let pushX = Math.random() * -30 + 15;
-    let pushR = -Math.random()* 10 +5;
+    let pushY = Math.random() * 30 - 10;
+    let pushR = -Math.random() * 10 + 5;
     item.style.setProperty('--push-y', pushY + 'px');
-    item.style.setProperty('--push-x', pushX + 'px');
     item.style.setProperty('--push-r', pushR + 'deg');
     // item.style.animationName = 'push-animation';
-    item.style.animation = 'push-animation 2s ease-in-out 0.5s infinite alternate';
+    item.style.animation = 'push-animation 2s ease-in-out 0s infinite alternate';
     console.log("start pushing");
 }
 function StartPushingAll() {
@@ -69,10 +76,10 @@ function StartPushingAll() {
     console.log("start pushing applied to all.");
 }
 
-function hitting(item){
-    let hitY = Math.random() * 10-5;
+function hitting(item) {
+    let hitY = Math.random() * 10 - 5;
     let hitX = Math.random() * 100 - 50;
-    let hitR = -Math.random()* 10 +5;
+    let hitR = -Math.random() * 10 + 5;
     item.style.setProperty('--hit-y', hitY + 'px');
     item.style.setProperty('--hit-x', hitX + 'px');
     item.style.setProperty('--hit-r', hitR + 'deg');
@@ -90,9 +97,11 @@ function StarthittingAll() {
 function missCall1() {
     notice1 = createNotification();
     notice1.classList.add('pushingNotice');
+    document.querySelector("h1").innerText = "16:41";
     clickButton.onclick = missCall2;
 }
 function missCall2() {
+    document.querySelector("h1").innerText = "16:45";
     notice2 = createNotification();
     notice3 = createNotification();
     notice4 = createNotification();
@@ -106,7 +115,8 @@ function missCall2() {
     clickButton.onclick = missCall3;
 }
 function missCall3() {
-//phone starts to be stretched
+    document.querySelector("h1").innerText = "16:59";
+    //phone starts to be stretched
     notice7 = createNotification();
     notice8 = createNotification();
     notice9 = createNotification();
@@ -115,12 +125,13 @@ function missCall3() {
     notice9.classList.add('pushingNotice');
     phone.classList.add('call-stretch-1');
     clickButton.onclick = missCall4;
-// StartPushingAll();
+    // StartPushingAll();
     // let bgOriginal = document.querySelector(".bodyDefault");
     // bgOriginal.classList.add('background');
 }
 function missCall4() {
-//change phone background and notice starts pushing ups and downs
+    document.querySelector("h1").innerText = "17:05";
+    //change phone background and notice starts pushing ups and downs
     notice10 = createNotification();
     notice11 = createNotification();
     notice10.classList.add('pushingNotice');
@@ -130,7 +141,8 @@ function missCall4() {
     StartPushingAll();
 }
 function missCall5() {
-//change the whole background and notices starts to hit the phone frames
+    document.querySelector("h1").innerText = "17:19";
+    //change the whole background and notices starts to hit the phone frames
     notice12 = createNotification();
     notice13 = createNotification();
     notice14 = createNotification();
@@ -144,109 +156,63 @@ function missCall5() {
 }
 function missCall6() {
     phone.remove();
-createBulletingNote();
-createBulletingNote();
-createBulletingNote();
-createBulletingNote();
-createBulletingNote();
-createBulletingNote();
-createBulletingNote();
-createBulletingNote();
-createBulletingNote();
-createBulletingNote();
-createBulletingNote();
-createBulletingNote();
-createBulletingNote();
-createBulletingNote();
-createBulletingNote();
-createBulletingNote();
-createBulletingNote();
-createBulletingNote();
-createBulletingNote();
-createBulletingNote();
-createBulletingNote();
-createBulletingNote();
-createBulletingNote();
-createBulletingNote();
-createBulletingNote();
-createBulletingNote();
-createBulletingNote();
-createBulletingNote();
-createBulletingNote();
-createBulletingNote();
-StartDriftingAll();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    createBulletingNote();
+    StartDriftingAll();
     let bgOriginal = document.querySelector(".bodyDefault");
     bgOriginal.classList.add('background');
 }
-function missCall7() {
-    createNotification();
-    clickButton.onclick = missCall8;
-}
-
-
-function missCall8() {
-    createNotification();
-    console.log("trigger bulge-belly");
-    phone.classList.add('bulge-belly-1');
-    // let bgOriginal = document.querySelector(".bodyDefault");
-    bgOriginal.classList.add('background');
-    //    phone.style.height+= phoneLength;
-    clickButton.onclick = missCall9;
-}
-
-function missCall9() {
-    createNotification();
-    console.log("trigger bulge-belly");
-    phone.classList.add('bulge-belly-2');
-    //    phone.style.height+= phoneLength;
-    clickButton.onclick = missCall10;
-}
-
-function missCall10() {
-    createNotification();
-    console.log("trigger bulge-belly");
-    phone.classList.add('bulge-belly-3');
-    //    phone.style.height+= phoneLength;
-    clickButton.onclick = missCall11;
-}
-
-function missCall11() {
-    createNotification();
-    createNotification();
-    createNotification();
-    createNotification();
-    createNotification();
-    createNotification();
-    createNotification();
-    createNotification();
-    console.log("trigger bulge-belly");
-    phone.classList.add('bulge-belly-4');
-    startdrifting();
-    //    phone.style.height+= phoneLength;
-    clickButton.onclick = missCall12;
-}
-
-function missCall12() {
-    // createNotification();
-    newNotification = createNotification();
-    newNotification = createNotification();
-    newNotification = createNotification();
-    startDrifting(newNotification);
-    console.log("trigger bulge-belly");
-    phone.classList.add('bulge-belly-5');
-    clickButton.onclick = missCall13;
-}
-
-// function missCall13(){
-// createNotification();
-// createNotification();
-// createNotification();
-// createNotification();
-// createNotification();
-// createNotification();
-// createNotification();
-// createNotification();
-//    console.log("trigger bulge-belly"); 
-//    phone.classList.add('bulge-belly-3');
-//    clickButton.onclick = missCall13;
-// }
