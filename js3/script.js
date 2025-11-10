360869ec2c6033d4c85de7d0313881963f7b8333
Wanyan Yu <wy2478@nyu.edu>
2025-11-10 18:39:02 +0800

    js-3

---
+++ js3/script.js
@@ -0,0 +1,17 @@
+let firstButton = document.querySelector("#firstButton");
+
+
+//we give an element to this function and let the function change it
+function colorize(elm){
+// elm.innerText = "started";
+elm.style.backgroundColor = "red";
+}
+
+function clickedButton() {
+let b = document.querySelector(".box");
+// colorize(b);
+console.log(b);
+let bs = document.querySelectorAll(".box");
+console.log(bs);
+bs.forEach(colorize);
+}
