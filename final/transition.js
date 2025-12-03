be1423e2270ea084b59ac3dbb908af8d5c0fface
Wanyan Yu <wy2478@nyu.edu>
2025-12-03 18:43:35 +0800

    after wed

--- final/transition.js
+++ final/transition.js
@@ -3,17 +3,26 @@ const follow = document.getElementById("follow");
 let lettersDropped = false;
 let grandpaImageShown = false;
 
+let mouseX = 100;
+let mouseY = 100;
+let grandpaImg;
+let controlingGrandpa = true;
+
 // Let a text "grandpa" appears on the cursor
 function handleMouseMove(e) {
-    follow.style.left = e.clientX + 10 + "px";
-    follow.style.top = e.clientY + 10 + "px";
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+
+    
+    follow.style.left = mouseX + 10 + "px";
+    follow.style.top = mouseY + 10 + "px";
 }
 
-// 显示 grandpa 并开始跟随
+
 follow.style.display = "block";
 document.addEventListener("mousemove", handleMouseMove);
 
-// 创建 8 屏高的渐变页面
+
 const page = document.createElement("div");
 page.className = "extra-page";
 body.appendChild(page);
@@ -29,7 +38,8 @@ townBg.appendChild(img);
 body.appendChild(townBg);
 
 // while scrolling the letters fall apart and disappear
-window.addEventListener("scroll", function () {
+window.addEventListener("scroll", function (e) {
+    console.log(e)
     const scrollTop = window.scrollY;
     const extraPageTop = page.offsetTop;
     const extraPageHeight = page.offsetHeight;
@@ -47,7 +57,7 @@ window.addEventListener("scroll", function () {
           const randomX = (Math.random() - 0.5) * spreadX;
           const randomY = minY + Math.random() * (maxY - minY);
       
-          letter.style.transition = "transform 6s ease-out, opacity 5s ease-out";
+          letter.style.transition = "transform 8s ease-out, opacity 7s ease-out";
           letter.style.transform = `translate(${randomX}px, ${randomY}px)`;
           letter.style.opacity = "0";
         });
@@ -55,22 +65,50 @@ window.addEventListener("scroll", function () {
     if (scrollProgress > 0.8) {
 
         follow.style.display = "none";
-        const grandpaImg = document.createElement("img");
-        grandpaImg.src = "assets/grandpa.png";
-        grandpaImg.alt = "grandpa";
-        grandpaImg.style.position = "fixed";
-        grandpaImg.style.pointerEvents = "none";
-        grandpaImg.style.width = "120px";
-        grandpaImg.style.height = "auto";
-        grandpaImg.style.transform = "translate(-50%, -50%)";
-        grandpaImg.style.zIndex = "999";
+        
+        if(grandpaImg == undefined){
+            grandpaImg = document.createElement("img");
+            grandpaImg.src = "assets/grandpa.png";
+            grandpaImg.alt = "grandpa";
+            grandpaImg.style.position = "fixed";
+            grandpaImg.style.pointerEvents = "none";
+            grandpaImg.style.width = "120px";
+            grandpaImg.style.height = "auto";
+            // grandpaImg.style.marginTop = "-100%"
+            grandpaImg.style.transform = "translate(-50%, -94%)";
+            grandpaImg.style.zIndex = "999";
+            grandpaImg.id="grandpaImg";
 
-        body.appendChild(grandpaImg);
+            body.appendChild(grandpaImg);
+        }
+        
+        
+        grandpaImg.style.left = mouseX + "px";
+        grandpaImg.style.top = mouseY + "px";
 
 
         document.addEventListener("mousemove", function moveGrandpa(e) {
-            grandpaImg.style.left = e.clientX + "px";
-            grandpaImg.style.top = e.clientY + "px";
+            if(controlingGrandpa == true){
+                grandpaImg.style.top = e.clientY + "px";
+                grandpaImg.style.left = e.clientX + "px";
+            
+            }
+
+            let greenY = window.innerHeight * 0.875;
+
+            if(scrollProgress > 0.99 && e.clientY > greenY){
+                controlingGrandpa = false;
+                grandpaImg.style.top = greenY + "px";
+            }
+            
+
+            // let greenY = window.innerHeight * 0.875;
+            // console.log(scrollProgress)
+            // if(e.clientY  < greenY){
+            //     grandpaImg.style.top = e.clientY + "px";
+            // }
+            // grandpaImg.style.left = e.clientX + "px";
+            
         });
     }
 });
